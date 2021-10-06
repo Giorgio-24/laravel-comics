703b48761923bb50f5d747af83727870bec27dc6
Giorgio Andrei <cipuandrei@libero.it>
2021-10-06 19:14:51 +0200

    footer done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <header class="container h-120 align-items-center">
+    <header class="container h-120 align-items-center pt-3">
         <div class="row justify-content-between">
           <figure class="clickable col-2 d-flex align-items-center">
             <img src="{{asset('images/dc-logo.png')}}" alt="dc-logo" class="img-fluid" />
@@ -25,44 +25,95 @@
           </div>
         </div>
       </header>
-{{-- <footer>
-    <section>
-        <div class="container h-100">
-          <div class="">
-            <a href="#" class="uppercase">Sign-up now!</a>
-          </div>
-          <div class="d-flex">
-            <ul class="d-flex">
-              <li><a href="#" class="uppercase">Follow us</a></li>
-              <li>
-                <a href="#">
-                    <img src="" alt="facebook-logo"/>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                    <img src="" alt="twitter-logo"/>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                    <img src="" alt="youtube-logo"/>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                    <img src="" alt="pinterest-logo"/>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                    <img src="" alt="periscope-logo"/>
-                </a>
-              </li>
-            </ul>
-          </div>
+      <footer>
+        <div class="footer-bg">
+          <section class="footer-list container">
+            <div class="d-flex">
+              <div>
+                <h3 class="uppercase">Dc comics</h3>
+                <ul>
+                  <li><a href="#">Characters</a></li>
+                  <li><a href="#">Comics</a></li>
+                  <li><a href="#">Movies</a></li>
+                  <li><a href="#">TV</a></li>
+                  <li><a href="#">Games</a></li>
+                  <li><a href="#">Videos</a></li>
+                  <li><a href="#">News</a></li>
+                </ul>
+                <h3 class="uppercase">Shop</h3>
+                <ul>
+                  <li><a href="#">Shop DC</a></li>
+                  <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+              </div>
+              <div>
+                <h3 class="uppercase">Dc</h3>
+                <ul>
+                  <li><a href="#">Therms Of Use</a></li>
+                  <li><a href="#">Privacy Policy (New)</a></li>
+                  <li><a href="#">Ad Choiches</a></li>
+                  <li><a href="#">Advertising</a></li>
+                  <li><a href="#">Jobs</a></li>
+                  <li><a href="#">Subscriptions</a></li>
+                  <li><a href="#">Talent Workshops</a></li>
+                  <li><a href="#">CPSC Certificates</a></li>
+                  <li><a href="#">Ratings</a></li>
+                  <li><a href="#">Shop Help</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div>
+                <h3 class="uppercase">Sites</h3>
+                <ul>
+                  <li><a href="#">DC</a></li>
+                  <li><a href="#">MAD Magazine</a></li>
+                  <li><a href="#">DC Kids</a></li>
+                  <li><a href="#">DC Universe</a></li>
+                  <li><a href="#">DC Power Visa</a></li>
+                </ul>
+              </div>
+            </div>
+          </section>
         </div>
-      </section>
-</footer> --}}
+        <section class="socials w-100">
+          <div class="container h-100 pt-4">
+            <div class="row align-items-center justify-content-center">
+              <div class="sign-up-box d-flex col-2">
+                <a href="#" class="text-uppercase fw-normal">Sign-up now!</a>
+              </div>
+              <div class="col-10 d-flex justify-content-end">
+                <ul class="d-flex align-items-center">
+                  <li><a href="#" class="text-uppercase">Follow us</a></li>
+                  <li>
+                    <a href="#">
+                        <img src="{{asset('images/footer-facebook.png')}}" alt="facebook-logo"/>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                        <img src="{{asset('images/footer-twitter.png')}}" alt="twitter-logo"/>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                        <img src="{{asset('images/footer-youtube.png')}}" alt="youtube-logo"/>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="{{asset('images/footer-pinterest.png')}}" alt="periscope-logo"/>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                        <img src="{{asset('images/footer-periscope.png')}}" alt="pinterest-logo"/>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+         </section>
+      </footer> 
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $nav_links = config('header_nav');
-    var_dump($nav_links);
     return view('home', compact('nav_links'));
 })->name('home');
